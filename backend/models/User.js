43f41966a353933e12e5e994b911ea5997dc7b57
Akshita Gupta <akshitaa.guptaaa@gmail.com>
2026-08-22 01:25:04 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -50,6 +50,14 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: true,
     },
+    resetPasswordToken: {
+      type: String,
+      select: false,
+    },
+    resetPasswordExpires: {
+      type: Date,
+      select: false,
+    },
   },
   {
     timestamps: true,
